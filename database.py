64b0b9e0da70dc48ef6768fe4e92133e090e656f
MAHDIATH89 <mahdiathhasan89@gmail.com>
2024-01-06 19:53:59 +0000

    updated and added database

---
+++ database.py
@@ -0,0 +1,5 @@
+import sqlalchemy
+from sqlalchemy import create_engine 
+
+
+engine=create_engine("mysql+pymysql://u6d8d1xka0w9d3psu57z:pscale_pw_gtdHlOEPInZI2ZnqUZP2WgdbpRQBPwJnRGnBpfjGgcU@ap-south.connect.psdb.cloud/joviancarear?charset=utf8mb4")
