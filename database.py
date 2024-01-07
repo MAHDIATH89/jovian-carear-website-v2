68139f49416b8c1c896bf6e75384fccb5e2b7f7a
Mahdiath Hasan <mahdiathhasan89@gmail.com>
2024-01-07 21:09:16 +0600

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,20 @@
 import sqlalchemy
-from sqlalchemy import create_engine 
+from sqlalchemy import create_engine, text
 
+db_connection_string = "mysql+pymysql://2uot6vwf2s9wwtp3smwt:pscale_pw_VIobgTOafYVF0bFUUhtag9PNgo2SAGJlSmYmydLkZPl@aws.connect.psdb.cloud/joviancarear?charset=utf8mb4"
 
-engine=create_engine("mysql+pymysql://u6d8d1xka0w9d3psu57z:pscale_pw_gtdHlOEPInZI2ZnqUZP2WgdbpRQBPwJnRGnBpfjGgcU@ap-south.connect.psdb.cloud/joviancarear?charset=utf8mb4")
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+with engine.connect() as conn:
+  result = conn.execute(text(" select * from jobs "))
+  print("type result",type(result))
+  result_all = result.all()
+  print("type result_all",type(result_all))
+print("result_all",result_all)
+
+first_result_dict = dict(result_all[0])
+print("type result dict :",type(first_result_dict))
+print(first_result_dict)  
